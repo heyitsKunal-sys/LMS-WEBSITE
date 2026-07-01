688210084a5747d14c9a13c368200d2e6ad9422f
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-01 16:09:22 +0530

    commit

---
+++ vite-project/src/context/AppContext.jsx
@@ -0,0 +1,15 @@
+import {createContext} from 'react';
+
+export const AppContext = createContext()
+
+export  const AppContextProvider = (props) =>{
+    const value ={
+
+    }
+    return (
+        <AppContextProvider value = {value}>
+            {props.children}
+        </AppContextProvider>
+    )
+
+}
---
+++ vite-project/src/pages/educator/StudentsEnroll.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StudentsEnroll = () => {
+  return (
+    <div>
+      <h1>Students Enroll Page</h1>
+    </div>
+  )
+}
+
+export default StudentsEnroll
