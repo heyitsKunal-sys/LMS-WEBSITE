690476a9573714a02625998f3d4f4239e7be59d1
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-02 23:22:04 +0530

    commit

--- vite-project/src/components/student/Hero.jsx
+++ vite-project/src/components/student/Hero.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-        <h1>Hero</h1>
+      <h1 className='md: text-home-heading-large-text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto'>Welcome to Our Platform.
+        <span className='text-blue-900'>Shaping Futures with Designed Courses and Expert Instruction
+
+        </span><img src="{assets.sketch}" alt="sketch" className='md:block hidden absolute -bottom-7 right-0'/>
+
+      </h1>
     </div>
   )
 }
--- vite-project/src/components/student/Navbar.jsx
+++ vite-project/src/components/student/Navbar.jsx
@@ -4,22 +4,22 @@ import { Link } from 'react-router-dom'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
- 
+
   const isCourseListPage = location.pathname.includes('/course-list');
-    const {openSignIn, openSignUp} = useClerk()
-    const {user} = useUser()
-    return (
+  const { openSignIn, openSignUp } = useClerk()
+  const { user } = useUser()
+  return (
     <div className={`flex justify-between items-center px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white-100' : 'bg-white'}`}>
       <img src={assets.logo} alt="Logo" className='w-10 lg:w-20 cursor-pointer' />
 
       <div className='hidden md:flex items-center gap-6 text-gray-700'>
         <div className='flex items-center gap-5'>
 
-          { user && 
-          <>
-            <button> Become Instructor | </button>
-          <Link to='/my-enrollments'>My Enrollments</Link>
-          </>
+          {user &&
+            <>
+              <button> Become Instructor | </button>
+              <Link to='/my-enrollments'>My Enrollments</Link>
+            </>
           }
           {/* we use && opeartor because we only want to display these elements if the user is logged in 
           we use <>...</> to group multiple elements 
@@ -27,11 +27,11 @@ const Navbar = () => {
         </div>
 
         {user ? <UserButton /> :
-          
-          
+
+
           <button onClick={() => openSignIn()} className='bg-purple-950 text-white px-5 py-2 rounded-3xl hover:bg-purple-900'>
-          Create Account</button>}
-          {/* here we use ternary operator because we want to display different elements based on the user's authentication status
+            Create Account</button>}
+        {/* here we use ternary operator because we want to display different elements based on the user's authentication status
           simply means if user exists, render UserButton, otherwise render Create Account button */}
 
       </div>
@@ -39,20 +39,26 @@ const Navbar = () => {
 
       {/* phone screen */}
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
-        <div>
-          { user && 
-          <>
-            <button> Become Instructor | </button>
-          <Link to='/my-enrollments'>My Enrollments</Link>
-          </>
+        <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+          {user &&
+            <>
+              <button> Become Instructor | </button>
+              <Link to='/my-enrollments'>My Enrollments</Link>
+            </>
           }
+          {/* when user is logged in then this become instructor and my enrollments will shown */}
         </div>
-        <button><img src={assets.user_icon} alt="User" /></button>
+        {
+          user ? <UserButton /> :
+            <button onClick={() => openSignIn()}>
+              <img src={assets.user_icon} alt="User" />
+            </button>
+        }
 
       </div>
     </div>
   )
-  
+
 }
 
 export default Navbar
