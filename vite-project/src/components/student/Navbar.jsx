8fafbecfb61f90ca70a9e24f24e3c40ff3e2ddfd
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-02 22:50:45 +0530

    commit

--- vite-project/src/components/student/Navbar.jsx
+++ vite-project/src/components/student/Navbar.jsx
@@ -14,8 +14,16 @@ const Navbar = () => {
 
       <div className='hidden md:flex items-center gap-6 text-gray-700'>
         <div className='flex items-center gap-5'>
-          <button> Become Instructor | </button>
+
+          { user && 
+          <>
+            <button> Become Instructor | </button>
           <Link to='/my-enrollments'>My Enrollments</Link>
+          </>
+          }
+          {/* we use && opeartor because we only want to display these elements if the user is logged in 
+          we use <>...</> to group multiple elements 
+          simply user && <> </> means if user exists, render the elements inside */}
         </div>
 
         {user ? <UserButton /> :
@@ -23,6 +31,8 @@ const Navbar = () => {
           
           <button onClick={() => openSignIn()} className='bg-purple-950 text-white px-5 py-2 rounded-3xl hover:bg-purple-900'>
           Create Account</button>}
+          {/* here we use ternary operator because we want to display different elements based on the user's authentication status
+          simply means if user exists, render UserButton, otherwise render Create Account button */}
 
       </div>
 
@@ -30,8 +40,12 @@ const Navbar = () => {
       {/* phone screen */}
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
         <div>
-          <button> Become Instructor | </button>
+          { user && 
+          <>
+            <button> Become Instructor | </button>
           <Link to='/my-enrollments'>My Enrollments</Link>
+          </>
+          }
         </div>
         <button><img src={assets.user_icon} alt="User" /></button>
 
