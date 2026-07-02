bf09caf2efe522c5cc806bd3ab3cdf10d8695e62
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-03 00:25:51 +0530

    commit

--- vite-project/src/components/student/Hero.jsx
+++ vite-project/src/components/student/Hero.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import {assets} from '../../assets/assets'
+import { assets } from '../../assets/assets'
 
 const Hero = () => {
   return (
     <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-linear-to-b from-white-100 to-white'>
 
-      <h1 className='md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto'>Welcome to Our Platform
-
-       <br></br> <span className='text-blue-900' >Shaping Futures with Designed Courses and Expert Instructors
-
-        </span><img src={assets.sketch} alt="sketch" className='md:block hidden absolute -bottom-7 right-0' />
-
+      <h1 className="md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-6xl mx-auto leading-tight">
+        Build your future with
+        <br />
+        <span className="text-blue-900 md:text-[48px] text-[32px] whitespace-nowrap">
+          expert instructors and best courses
+        </span>
+        <img
+          src={assets.sketch}
+          alt="sketch"
+          className="md:block hidden absolute -bottom-7 right-0"
+        />
       </h1>
       <p className='md:block hidden text-gray-500 max-w-2xl mx-auto'>We have best courses , excellent teachers and instructors, interactive learning experience and a
         supportive community to help you achieve your goals. Join us today and start your journey towards success
