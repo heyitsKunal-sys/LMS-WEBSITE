a42007cf79e50977a600572321f22c1f50f47928
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-04 22:51:41 +0530

    commit

--- vite-project/src/components/student/TestimonialsSection.jsx
+++ vite-project/src/components/student/TestimonialsSection.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const TestimonialsSection = () => {
   return (
     <div>
-      <h1>Testimonials</h1>
+      <h2>Testimonials</h2>
+      <p>Journey of transformation and success of our learners as they 
+        share <br/> and how our platform has made a differencce in their lives. </p>
     </div>
   )
 }
