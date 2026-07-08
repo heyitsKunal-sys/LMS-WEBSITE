643b492ba99e976da476264b941baa83efc982f8
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-08 13:02:56 +0530

    commit

--- vite-project/src/pages/student/CourseDetails.jsx
+++ vite-project/src/pages/student/CourseDetails.jsx
@@ -1,12 +1,14 @@
-import React, { useContext, useState , useEffect } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../../context/AppContext'
+import Loading from '../../components/student/Loading'
+import { assets } from '../../assets/assets'
 
 const CourseDetails = () => {
   const { id } = useParams()   //using this id we can find particular course from allCourses
   const [courseData, setCourseData] = useState(null)
 
-  const { allCourses } = useContext(AppContext)
+  const { allCourses,calculateRating } = useContext(AppContext)
 
   const fetchCourseData = async () => {
     const findCourse = allCourses.find(course => course._id === id)
@@ -14,28 +16,47 @@ const CourseDetails = () => {
   }
   useEffect(() => {
     fetchCourseData()
-  
-    
+
+
   }, [])
-  
-  return (
-    <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start
+
+  return courseData ? (
+    <>
+      <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start
     justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
 
-      <div className='absolute top-0 left-0 w-full h-96  -z-1 bg-linear-to-b from-cyan-100/70'>
+        <div className='absolute top-0 left-0 w-full h-section-heights  -z-1 bg-linear-to-b from-cyan-100/70'>
 
+        </div >
+        {/* left coloumns */}
+        <div className='max-w-xl z-10 text-gray-600'>
+          <h1 className='md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800'>{courseData.courseTitle}</h1>
+          <p className='pt-4 md:text-base text-sm'
+            dangerouslySetInnerHTML={{ __html: courseData.courseDescription }}></p>
+          {/* review AND rating */}
+          <div className='flex items-center space-x-2'>
+
+            <p>{calculateRating(courseData)}</p>
+
+            <div className='flex '>
+              {[...Array(5)].map((_, i) => (
+                <img className='w-3.5 h-3.5' key={i} src={i < Math.floor(calculateRating(courseData)) ? assets.star : assets.star_blank} alt='' />)
+              )}
+
+            </div>
+            <p className='text-gray-500'>{courseData.courseRatings.length}</p>
+          </div>
+        </div>
+
+
+
+
+
+        {/* rightcoloumns */}
+        <div></div>
       </div>
-      {/* left coloumns */}
-      <div></div>
-
-
-
-
-
-      {/* rightcoloumns */}
-      <div></div>
-    </div>
-  )
+    </>
+  ) : <Loading />
 }
 
 export default CourseDetails
