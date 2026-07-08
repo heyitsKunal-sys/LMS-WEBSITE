003dd78382bbbf27bb531b0b67c2f92e84c7e09e
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-08 11:07:21 +0530

    commit

--- vite-project/src/pages/student/CourseDetails.jsx
+++ vite-project/src/pages/student/CourseDetails.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useContext, useState , useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
 
 const CourseDetails = () => {
+  const { id } = useParams()   //using this id we can find particular course from allCourses
+  const [courseData, setCourseData] = useState(null)
+
+  const { allCourses } = useContext(AppContext)
+
+  const fetchCourseData = async () => {
+    const findCourse = allCourses.find(course => course._id === id)
+    setCourseData(findCourse);
+  }
+  useEffect(() => {
+    fetchCourseData()
+  
+    
+  }, [])
+  
   return (
-    <div>
-      <h1>Course Details Page</h1>
+    <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start
+    justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
+
+      <div className='absolute top-0 left-0 w-full h-96  -z-1 bg-linear-to-b from-cyan-100/70'>
+
+      </div>
+      {/* left coloumns */}
+      <div></div>
+
+
+
+
+
+      {/* rightcoloumns */}
+      <div></div>
     </div>
   )
 }
